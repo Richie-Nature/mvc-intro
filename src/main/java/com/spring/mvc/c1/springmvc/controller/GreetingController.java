0ce5e5338f6c2dd7e67a38396b39e4807950373a
Imoh Obot <obotimoh20@gmail.com>
2021-10-22 14:13:50 +0100

    thymeleaf foreach and unless..hmm much like frontend templating engines

--- src/main/java/com/spring/mvc/c1/springmvc/controller/GreetingController.java
+++ src/main/java/com/spring/mvc/c1/springmvc/controller/GreetingController.java
@@ -6,13 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.*;
+
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World")
                                    String name, Model model) {
+        List<String> greetings = new LinkedList<>();
+        greetings.add("Hello");
+        greetings.add("Hey there!");
+        greetings.add("Hi!");
+        greetings.add("Goodbye");
+
         model.addAttribute("name", name);
+        model.addAttribute("greetings", greetings);
         return "greet";
     }
 
