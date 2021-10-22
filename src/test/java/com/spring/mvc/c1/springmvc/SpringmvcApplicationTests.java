d15176f5b1e230bec65c73303a9f164af8f70dee
Imoh Obot <obotimoh20@gmail.com>
2021-10-22 12:43:13 +0100

    initial commit

---
+++ src/test/java/com/spring/mvc/c1/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mvc.c1.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
